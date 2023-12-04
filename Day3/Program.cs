327d61712975a8058fec0675211683472fa1be11
Oskar Selberg <oskar.selberg@stenaline.com>
2023-12-04 02:29:44 +0100

    solve day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,6 +1,10 @@
-﻿var inputFileContent = File.ReadAllLines("Input.txt");
-Console.WriteLine("First: " + First(inputFileContent) + "(<<559667>>)");
+﻿using System.Diagnostics.CodeAnalysis;
 
+var inputFileContent = File.ReadAllLines("Input.txt");
+Console.WriteLine("First: " + First(inputFileContent) + "(<<559667>>)");
+Console.WriteLine("Second: " + Second(inputFileContent) + "(<<86841457>>)");
+
+#region part1
 int First(string[] input)
 {
     var numberHits = new List<int>();
@@ -50,7 +54,7 @@ bool[,] CalculateHitMatrix(string[] schematics, Predicate<char> predicate)
         {
             if (predicate(element.element))
             {
-               SetHitsAroundCharacter(symbolHits, element.x, y);
+                SetHitsAroundCharacter(symbolHits, element.x, y);
             }
         }
     }
@@ -61,9 +65,92 @@ bool[,] CalculateHitMatrix(string[] schematics, Predicate<char> predicate)
 static void SetHitsAroundCharacter(bool[,] hitMatrix, int x, int y)
 {
     for (var yOffset = -1; yOffset <= 1; yOffset++)
-        for (var xOffset = -1; xOffset <= 1; xOffset++)
-            hitMatrix[x + xOffset, y + yOffset] = true;
+    for (var xOffset = -1; xOffset <= 1; xOffset++)
+        hitMatrix[x + xOffset, y + yOffset] = true;
 }
+
+
+#endregion
+
+int Second(string[] input)
+{
+    var gears = GetAllGears(input); 
+    var numbers = GetAllNumbers(input);
+    
+    foreach (var gear in gears)
+    {
+        foreach (var number in numbers)
+        {
+           if (number.X.Any(x => gear.IsAdjacentToCell(x, number.Y)))
+               gear.AdjacentNumbers.Add(number.Value);
+        }
+    }
+    
+    return gears.Where(x => x.IsValid).Select(x => x.GearValue).Sum();
+}
+
+List<Gear> GetAllGears(string[] input)
+{
+    var gears = new List<Gear>();
+    
+    foreach (var (line, y) in input.Select((l, y) => (l, y)))
+    {
+        foreach (var (element, x) in line.Select((e, x) => (e, x)))
+        {
+            if (element == '*') gears.Add(new Gear{X = x, Y = y});
+        }
+    }
+
+    return gears;
+}
+
+List<Number> GetAllNumbers(string[] input)
+{
+    var numbers = new List<Number>();
+
+    foreach (var (row, y) in input.Select((r, y) => (r, y)))
+    {
+        var potentialNumberIndexes = new List<int>();
+        var potentialNumber = "";
+        
+        foreach (var (element, x) in row.Select((e, x) => (e, x)))
+        {
+            if (char.IsDigit(element))
+            {
+                potentialNumberIndexes.Add(x);
+                potentialNumber += element;
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(potentialNumber)) continue;
+                numbers.Add(new Number(y, potentialNumberIndexes.Select(v => v).ToList(), int.Parse(potentialNumber)));
+        
+                potentialNumberIndexes.Clear();
+                potentialNumber = "";
+            }
+        }
+        
+        if (!string.IsNullOrEmpty(potentialNumber))
+            numbers.Add(new Number(
+                y, 
+                potentialNumberIndexes.Select(v => v).ToList(), 
+                int.Parse(potentialNumber)));;
+        
+    }
+
+    return numbers;
+}
+
+internal class Gear
+{
+    public int X { get; set;  }
+    public int Y { get; set; }
+    public List<int> AdjacentNumbers { get; set; } = new ();
+    public bool IsValid => AdjacentNumbers.Count == 2;
+    public int GearValue => AdjacentNumbers.Aggregate((sum, val) => sum * val);
+    public bool IsAdjacentToCell(int x, int y) => Math.Abs(X - x) <= 1 && Math.Abs(Y - y) <= 1;
+}
+internal record struct Number(int Y, List<int> X, int Value) { }
 
 
 
